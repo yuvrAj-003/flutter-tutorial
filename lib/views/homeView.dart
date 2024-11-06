466aa10a12e154fd758cc0e4257c7814b9d838a2
YUVRAJ SINGH <yuvrajsingh@YUVRAJs-MacBook-Pro.local>
2024-11-06 19:51:20 +0530

    changes made

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/views/calculator/calcView.dart';
+// import 'package:flutter_application_1/views/calculator/calcView.dart';
 import 'package:flutter_application_1/views/ice_cream/IcecreamView.dart';
 
 void main(){
